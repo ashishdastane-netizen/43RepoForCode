86e82250eb6d5012fadea35b77648846c20488d5
ashishdastane-netizen <ashish.dastane@gmail.com>
2026-04-23 15:30:00 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, World! This is Ashish Dastane on 23rd April Thursday post afternoon session with class friends like Prince, Himanshu, Vikas Sir etc......");
-        System.out.println("Dastane Family Yogini, Kush, Varsha Dastane, Shashikant Dastane");
+        System.out.println("Dastane Family Yogini, Kush, Varsha Dastane, Shashikant Dastane!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
 }
